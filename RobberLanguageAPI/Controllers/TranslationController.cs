ae59f340e1e80f1ea06f11e78a95767aecd92cbc
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-04-21 10:42:34 +0200

    Add project files.

---
+++ RobberLanguageAPI/Controllers/TranslationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RobberLanguageAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static RobberLanguageAPI.Models;
+
+namespace RobberLanguageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        public static string TranslateSentence(string input)
+        {
+            string translatedSentence = "";
+            char[] vowels = { 'a', 'o', 'u', 'å', 'e', 'i', 'y', 'ä', 'ö', ' ' }; 
+
+            foreach (char letter in input)
+            {
+                for (int i = 0; i < vowels.Length; i++)
+                {
+                    if (letter == vowels[i])
+                    {
+                        translatedSentence += letter;
+                    }
+                    else if (letter == 'x')
+                    {
+                        translatedSentence += "koksos";
+                    }
+                    else
+                    {
+                        translatedSentence += letter + 'o' + letter;
+                    }
+                }
+            }
+
+            return translatedSentence;
+        }
+    }
+}
---
+++ RobberLanguageAPI/Models/Translation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobberLanguageAPI.Models
+{
+    public class Translation
+    {
+        public string OrginalSentence { get; set; }
+        public string TranslatedSentence { get; set; }
+    }
+}
